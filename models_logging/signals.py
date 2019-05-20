d1692ee3f99ca137fcaa5f4d354737a38f5676df
Eugene Kovalev <yevgeniy.kovalev@leverx.com>
2019-05-20 12:14:57 +0300

    [+] added support for divide_on_update

--- models_logging/signals.py
+++ models_logging/signals.py
@@ -45,17 +45,43 @@ def delete_model(sender, instance, using, **kwargs):
             _create_changes(instance, using, DELETED)
 
 
-def _create_changes(object, using, action):
-    changed_data = json.dumps(get_changed_data(object, action), cls=JSON_ENCODER)
+def _create_changes(instance, using, action):
+    """
+    Creates a `Change` instance
+    :param instance: an object which undergone the "action"
+    :param using: a database associated with the object
+    :param action: action performed to the object `created`/`changed`/`deleted`
+    """
+    changed_data = json.dumps(get_changed_data(instance, action), cls=JSON_ENCODER)
     user_id = _local.user.pk if _local.user and _local.user.is_authenticated else None
-    content_type_id = ContentType.objects.get_for_model(object._meta.model).pk
-    data = {'db': using, 'object_repr': force_text(object), 'action': action, 'user_id': user_id,
-            'changed_data': changed_data, 'object_id': object.pk, 'content_type_id': content_type_id}
+    content_type_id = ContentType.objects.get_for_model(instance._meta.model).pk
+
+    data = {
+        'db': using,
+        'object_repr': force_text(instance),
+        'action': action,
+        'user_id': user_id,
+        'changed_data': changed_data,
+        'object_id': instance.pk,
+        'content_type_id': content_type_id,
+    }
+
     if MERGE_CHANGES and 'models_logging.middleware.LoggingStackMiddleware' in MIDDLEWARES:
-        key = (object.pk, content_type_id)
+        key = (instance.pk, content_type_id)
         old_action = _local.stack_changes.get(key, {}).get('action')
         if old_action == ADDED:
             data['action'] = ADDED
         _local.stack_changes[key] = data
     else:
+        if data['action'] == CHANGED:
+            divide_on_update = getattr(getattr(instance, 'ModelLogging', object), 'divide_on_update', False)
+            if divide_on_update:
+                Change.objects.bulk_create([
+                    Change(**{
+                        **data,
+                        **{'changed_data': json.dumps([_])}
+                    }) for _ in json.loads(data['changed_data'])
+                ])
+                return
+
         Change.objects.create(**data)
