0cb0c53fda698d28360d9024320137d8b1230be3
Andrii <andrii@gwapit.com>
2021-10-21 22:58:38 +0300

    version 1.0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ files = ["templates/models_logging/*", "migrations/*", "management/commands/*"]
 
 setup(
     name='django-models-logging',
-    version='1.0.7',
+    version='1.0.8',
     packages=['models_logging'],
     url='https://github.com/legion-an/django-models-logging',
     package_data={'models_logging' : files},
